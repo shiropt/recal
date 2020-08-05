6d196b017fd3498e1f12f35a286f4b6535ac801e
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-05 17:45:10 +0900

    MenuList.vue作成

---
+++ src/components/MenuList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Menu
+    morningMenu="パン"
+    lunchMenu="ラーメン"
+    dinnerMenu="そば"
+    date="6月10日"
+    />
+    <Menu
+    morningMenu="パン"
+    lunchMenu="ラーメン"
+    dinnerMenu="そば"
+    date="6月11日"
+
+    />
+    <Menu
+    morningMenu="パン"
+    lunchMenu="ラーメン"
+    dinnerMenu="そば"
+    date="6月12日"
+
+    />
+
+  </div>
+</template>
+
+<script>
+import Menu from "@/components/Menu.vue"
+
+export default {
+  components:{
+    Menu
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
