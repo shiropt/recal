65212c7257caf7490fa1d186314a908798f06738
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-05 17:58:34 +0900

    テンプレート作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an home page</h1>
+  <div class="menu-list">
+    <h2>最近のレシピ一覧</h2>
+    <MenuList
+    class="menu-list" />
   </div>
 </template>
+
+<script>
+import MenuList from "@/components/MenuList.vue"
+export default {
+  components:{
+    MenuList
+
+  }
+}
+</script>
+
+<style>
+.menu-list{
+  width: 700px;
+  margin: 0 auto;
+}
+
+</style>
