b024a76c5507023a8b746dd9730a390790f5cfe7
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-05 18:41:27 +0900

    axios導入

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,21 @@
 </template>
 
 <script>
+const axios = require('axios').default;
+import "axios"
 import MenuList from "@/components/MenuList.vue"
 export default {
   components:{
     MenuList
 
+  },
+  async mounted(){
+    try{
+      const rankRes = await axios.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&formatVersion=1&applicationId=1019675112210690221')
+      console.log(rankRes.data)
+    }catch(error){
+      alert(error.message)
+    }
   }
 }
 </script>
