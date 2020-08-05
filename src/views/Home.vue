5b0f2f1f200ab6826f1ceedb17f116a39292046f
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-05 18:56:15 +0900

    楽天レシピAPI取得

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,15 @@
     <h2>最近のレシピ一覧</h2>
     <MenuList
     class="menu-list" />
+  <ul>
+
+  <template v-for="(rank,index) in rankInfo">
+   <li
+   :key="index"
+   > {{rank.recipeTitle}}</li>
+  </template>
+  </ul>
+
   </div>
 </template>
 
@@ -15,10 +24,17 @@ export default {
     MenuList
 
   },
+  data(){
+    return{
+      rankInfo:null
+    }
+
+  },
   async mounted(){
     try{
       const rankRes = await axios.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&formatVersion=1&applicationId=1019675112210690221')
-      console.log(rankRes.data)
+      console.log(rankRes.data.result)
+      this.rankInfo= rankRes.data.result;
     }catch(error){
       alert(error.message)
     }
