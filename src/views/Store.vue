763a5d26419f2f34b81c372874950968e021c070
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-06 15:05:04 +0900

    店舗検索仮実装

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="profile">
     <h1>外食先を選ぶ</h1>
-    <!-- <Form
-    menu="外食先"/> -->
+    <StoreForm
+    :inClick="searchStore" />
+    <ul>
+    <template v-for="(store,index) in storeInfo">
+    <li :key="index">{{store.name}}</li>
+    </template>
+
+    </ul>
 
   </div>
 </template>
@@ -10,30 +16,37 @@
 <script>
 const axios = require('axios').default;
 import "axios"
-// import Form from "@/components/Form.vue"
+import StoreForm from "@/components/StoreForm.vue"
 
 export default {
-  // components:{
-  //   Form
-  // },
+  components:{
+    StoreForm
+  },
   data(){
     return{
       storeInfo:null
     }
 
   },
-  async mounted(){
-    try{
-      const storeRes = await axios
-      .get("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=720039c5c559d5c97319281d7cc3e669", {
-          params: {
-            freeword:"SAMA"
+  methods:{
+    async searchStore(input,address,buffet){
+      try{
+        if(buffet){
+      buffet=1
+    }
+        const storeRes = await axios
+        .get("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=720039c5c559d5c97319281d7cc3e669", {
+            params: {
+              freeword:input,
+              address:address,
+              buffet:buffet
+            }})
+       await  console.log(storeRes.data.rest)
+        this.storeInfo= storeRes.data.rest;
+      }catch(error){
+        alert(error.message)
+      }
 
-          }})
-     await  console.log(storeRes)
-      // this.rankInfo= rankRes.data.result;
-    }catch(error){
-      alert(error.message)
     }
   }
 }
