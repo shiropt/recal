26670482c309cc2c52111b9942102d7ac861feac
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-11 19:03:07 +0900

    HomeView修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,17 @@
 <template>
   <div class="menu-list">
     <h2>最近のレシピ一覧</h2>
-    <MenuList
-    class="menu-list" />
-  <ul>
-
-  <template v-for="(rank,index) in rankInfo">
-   <li
-   :key="index">
-    {{rank.recipeTitle}}
-    </li>
-  </template>
-  </ul>
-
+    <!-- <MenuList
+    class="menu-list" /> -->
   </div>
 </template>
 
 <script>
-const axios = require('axios').default;
-import "axios"
-import MenuList from "@/components/MenuList.vue"
+
+// import MenuList from "@/components/MenuList.vue"
 export default {
   components:{
-    MenuList
+    // MenuList
 
   },
   data(){
@@ -31,19 +20,11 @@ export default {
     }
 
   },
-  async mounted(){
-    try{
-      const rankRes = await axios.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&formatVersion=1&applicationId=1019675112210690221')
-      console.log(rankRes.data.result)
-      this.rankInfo= rankRes.data.result;
-    }catch(error){
-      alert(error.message)
-    }
-  }
+  
 }
 </script>
 
-<style>
+<style scoped>
 .menu-list{
   width: 700px;
   margin: 0 auto;
