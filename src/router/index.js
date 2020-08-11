83f9e3271e8a53c7ca2414d006c765e24507c63a
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-11 19:04:24 +0900

    ルーティング修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Search from '../views/Search.vue'
+import Store from '../views/Store.vue'
 
 Vue.use(VueRouter)
 
@@ -13,14 +15,18 @@ Vue.use(VueRouter)
   {
     path: '/search',
     name: 'Search',
-    
-    component: () => import('../views/Search.vue')
-    },
+    component: Search
+  },
   {
     path: '/store',
     name: 'Store',
+    component: Store
+    },
+  {
+    path: '/login',
+    name: 'Login',
     
-    component: () => import('../views/Store.vue')
+    component: () => import('../views/Login.vue')
     },
     {
       path: "*",
