648cf28baa4fca783560484e67b3c691fb21fd1d
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-13 09:20:03 +0900

    ルーティング修正

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Search from '../views/Search.vue'
-import Store from '../views/Store.vue'
+import Store from '@/views/Select/Store.vue'
+import Recipe from '@/views/Select/Recipe.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      beforeEnter: (to, from, next) => {
+        console.log('route(root):⊂(´ーωー｀)⊃r');
+        next();
+      }
+      },
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/search',
     name: 'Search',
-    component: Search
+    component: Search,
+    beforeEnter: (to, from, next) => {
+      console.log('route(Search):(´・ω・｀)ショボーン');
+      next();
+    },
+    children: [
+      {
+        path: 'store',
+        name: 'Store',
+        component: Store,
+        beforeEnter: (to, from, next) => {
+          console.log('route(Store)');
+          next();
+        },
+      },
+      {
+        path: 'recipe',
+        name: 'Recipe',
+        component: Recipe,
+        beforeEnter: (to, from, next) => {
+          console.log('route(Recipe)');
+          next();
+        },
+      }
+    ]
   },
-  {
-    path: '/store',
-    name: 'Store',
-    component: Store
-    },
-  {
-    path: '/login',
-    name: 'Login',
-    
-    component: () => import('../views/Login.vue')
-    },
     {
       path: "*",
       redirect:"/"
