4fb05d3ea14cc842d32c4b2adab93b1474e184e9
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-20 11:22:22 +0900

    headerリファクタリング

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,12 @@
 <template v-slot:header>
-        <v-toolbar
-          dark
-          color="orange"
-        >
-        <v-toolbar-title>
+  <v-toolbar  dark color="orange">
+   <v-toolbar-title>
       <router-link to="/" class="title" tag="h1" >Recal</router-link>
    </v-toolbar-title>
-     
    <v-spacer></v-spacer>
-          <v-text-field
-            v-model="search"
-            flat
-            solo-inverted
-            hide-details
-            label="Search"
-          ></v-text-field>
-         
+    <router-link to="/login" >Logout</router-link>
    <v-spacer></v-spacer>
-      <router-link to="/login" >Logout</router-link>
-   <v-spacer></v-spacer>
-      
-
-        </v-toolbar>
-      </template>
+  </v-toolbar>
+</template>
       
       
-      <script>
-      export default {
-         data(){
-            return {
-               search:""
-         }
-      
-      }
-      }
-      </script>
-      
-      <style>
-      
-      </style>
