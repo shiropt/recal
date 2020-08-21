b6c573ce3744fd76b945a3af51cc1f0112e3a81a
shiropt <63380875+shiropt@users.noreply.github.com>
2020-08-21 12:38:35 +0900

    ログアウトボタン修正

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,28 @@
 <template v-slot:header>
   <v-toolbar  dark color="orange">
    <v-toolbar-title>
-      <router-link to="/" class="title" tag="h1" >Recal</router-link>
+      <router-link to="/search" class="title" tag="h1" >Recal</router-link>
    </v-toolbar-title>
    <v-spacer></v-spacer>
-    <router-link to="/login" >Logout</router-link>
+   <v-btn v-if="this.$store.state.user.authState" @click="logout">Logout</v-btn>
    <v-spacer></v-spacer>
   </v-toolbar>
 </template>
+<script>
+import firebase from 'firebase'
+export default {
+   methods:{
+   async logout(){
+      try{
+         await firebase.auth().signOut()
+          this.$store.commit("authState")
+         this.$router.push('/')
+         alert("ログアウトしました")
+      }catch(error){
+         alert(error.message);
+      }
       
-      
+    }
+   }
+}
+</script>
