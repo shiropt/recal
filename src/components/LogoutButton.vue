9e3ea2eb9a4dbdfa4c2ba41550abc4bae0adf778
shiropt <63380875+shiropt@users.noreply.github.com>
2020-09-03 09:05:42 +0900

    LogoutButtonコンポーネント化

---
+++ src/components/LogoutButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn outlined class="logout" @click="logout">Logout</v-btn>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  methods:{
+    async logout(){
+    try{
+      await firebase.auth().signOut()
+      this.$store.commit("authState")
+      this.$router.push('/')
+   }catch(error){
+      alert(error.message);
+      }
+    },
+  }
+
+}
+</script>
+
+<style>
+
+</style>
