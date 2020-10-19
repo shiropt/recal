3865e64eb8fca1650552e734c858eb411d4a4e9d
shiropt <63380875+shiropt@users.noreply.github.com>
2020-10-19 10:56:45 +0900

    ファーストビュー修正

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -1,49 +1,52 @@
 <template>
  <div class="container">
-       <div class="side-bar">
-        <h1>Recalへようこそ</h1>
-        <h4>あなたの食事をRecalで管理しませんか？</h4>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-btn 
-        color="success"
-        >
-           <router-link :to="{name: 'Signup', params:{id:$route.params.id} }" tag="a">新規登録</router-link>
-        </v-btn><br>
-        <v-btn
-        color="warning"
-        >
-          <router-link :to="{name: 'Signin', params:{id:$route.params.id} }" tag="a">ログイン</router-link>
-          </v-btn>
-       </div>
-
-    
+        <img class="top-image" :src="imgPath" alt="TOPIMAGE" width="100%" height="100%">
+        <div class="top-message">
+          
+          <h2 class="message">日々の食事を管理</h2>
+          <h3 class="message">今日の献立はどうしよう、外食にする？</h3>
+          <h3 class="message">
+          迷った時にはRecalで検索！
+          </h3>
+        </div>
        <div class="main">
          <transition name="auth">
          <router-view></router-view>
          </transition>
        <transition name="fade">
            <div v-if="initialAccess">
-             <div class="title">
-             <h1>Recalで出来る事</h1>
+             <div class="description">
+             <h2>Recalとは</h2>
+             <p>あなたの食事内容を記録して管理します<br>
+               また、食事を考える時にレシピや飲食店の検索ができるサイトです</p>
              </div>
-             <ul>
-               <li>
-                 <h2>日々の食事の管理</h2>
-                 <p>毎日の食事を記録して栄養のバランスを考えよう！</p>
-               </li>
-               <li>
-                 <h2>献立を考える手助けをします</h2>
-                 <p>毎日献立を考えるのは大変...</p>
-                 <p>ピックアップメニューやレシピ検索を参考にしよう！</p>
-               </li>
-               <li>
-                 <h2>外食もありかな？</h2>
-                 <p>外食先もRecalで検索して探そう！</p>
-               </li>
-             </ul>
-             <h2>是非Recalを活用して毎日の食事を楽しんでください！</h2>
+             <div class="benefit">
+               <h2>Recalで できること</h2>
+               <div class="function-list">
+                 <div class="function">
+                   <p class="function-title">食事内容を記録</p>
+                   <div class="function-description">
+                     <p>朝食、昼食、夕食の内容を記録できます。是非毎日の食事内容を記録して栄養のバランスを考えるキッカケにしてみてください。</p>
+                   </div>
+                 </div>
+                 <div class="function">
+                   <p class="function-title">レシピを検索</p>
+                   <div class="function-description">
+                     <p>レシピの検索ができます。何を作ろうか迷った時にはピックアップメニューをチェック！</p>
+                   </div>
+                 </div>
+                 <div class="function">
+                   <p class="function-title">飲食店を検索</p>
+                   <div class="function-description">
+                     <p>外食をしようと思った時には飲食店を検索！目的の地域とフリーワードを入力して行きたいお店を探してみてください。
+                     </p>
+                   </div>
+                 </div>
+
+               </div>
+             </div>
+
+             
            </div>
          </transition>
         
@@ -53,9 +56,14 @@
 
 <script>
   export default {
-    data: () => ({
-    initialAccess:true
-    }),
+    data(){
+      return{
+        initialAccess:true,
+        imgPath: require('@/assets/top.png')
+
+
+      }
+    },
     methods:{
       createTopPage(){
         if(this.$route.path==="/"){
@@ -108,10 +116,23 @@
   opacity: 0;
 }
 
- .title {
-  position: relative;
-  padding: 0.25em 0;
-  text-shadow: 1px 1px 0 rgba(0,0,0,.5);
+ .description {
+   width: 100%;
+   height: 200px;
+   line-height: 50px;
+   margin: 0 auto;
+   text-align: center;
+   background: white;
+   font-size: 1.2rem;
+   font-weight:bold;
+}
+.benefit{
+  width: 100%;
+  height: 350px;
+  padding: 48px 0px;
+  text-align: center;
+  background-color: #edf2f5;;
+;
 }
 .title:after {
   content: "";
@@ -121,14 +142,25 @@
   background: linear-gradient(to right, rgb(255, 186, 115), #ffb2b2);
 }
 .container{
-  display: flex;
-  margin: 2%;
+  position: relative;
+  max-width: 100%;
+  padding: 0;
+  margin: 0;
+ 
 }
-.side-bar{
-  height: 100%;
-text-align: center;
+.top-message{
+  position:absolute;
+  top: 40px;
+  left:50px;
 }
+.message{
+  color: #ffff;
+    font-size: 2rem;
+    font-family: sans-serif;
+      text-shadow: 1px 1px 2px black;
+     box-shadow: 1 1 1re
 
+}
 .v-btn{
   margin-top: 38px;
   padding: 0px;
@@ -149,12 +181,25 @@ a {
 
 }
 
-.main{
-  height: 100%;
+.top-image{
+  opacity: 0.8;
 }
-
 li{
 
 list-style: none;
+}
+.function-list{
+  display: flex;
+  margin-top: 50px;
+}
+.function{
+  width: 30%;
+  margin: 0 30px;
+}
+.function-title{
+  border: solid 3px orange;
+  padding: 0.5em;
+  border-radius: 0.5em;
+
 }
 </style>
